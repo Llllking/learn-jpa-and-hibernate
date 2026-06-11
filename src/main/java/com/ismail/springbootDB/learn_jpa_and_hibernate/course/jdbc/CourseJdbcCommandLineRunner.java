ecea3e29d07e787015baee66f8d16dbfb8673968
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-06-11 18:26:03 +0100

    make INERT DELETE SELECT dynamic

--- src/main/java/com/ismail/springbootDB/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/ismail/springbootDB/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.ismail.springbootDB.learn_jpa_and_hibernate.course.jdbc;
 
+import com.ismail.springbootDB.learn_jpa_and_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,10 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        courseJdbcRepository.insert();
+        courseJdbcRepository.insert(new Course(1, "Learn AWS", "Ismail"));
+        courseJdbcRepository.insert(new Course(2, "Learn AZURE", "Ismail"));
+        courseJdbcRepository.insert(new Course(3, "Learn DevOps", "Ismail"));
+        courseJdbcRepository.delete(2);
+        System.out.println(courseJdbcRepository.selectById(1));
     }
 }
--- src/main/java/com/ismail/springbootDB/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/ismail/springbootDB/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,6 +1,8 @@
 package com.ismail.springbootDB.learn_jpa_and_hibernate.course.jdbc;
 
+import com.ismail.springbootDB.learn_jpa_and_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -9,13 +11,32 @@ public class CourseJdbcRepository {
     @Autowired
     private JdbcTemplate springJdbcTemplate;
 
-    private String sqlQuery =
+    private String INSERT_QUERY =
         """
             insert into course(id, name, author)
-            values (1, 'learn AWS', 'Ismail');
+            values (?, ?, ?);
         """;
 
-    public void insert(){
-        springJdbcTemplate.update(sqlQuery);
+    private static String DELETE_QUERY =
+            """
+                delete from course where id = ?
+            """;
+
+    private static String SELECT_BY_ID_QUERY =
+            """
+                select * from course where id = ?
+            """;
+
+    public void insert(Course course){
+        springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
     }
+
+    public void delete(long id){
+        springJdbcTemplate.update(DELETE_QUERY, id);
+    }
+
+    public Course selectById(long id){
+        return springJdbcTemplate.queryForObject(SELECT_BY_ID_QUERY, new BeanPropertyRowMapper<>(Course.class), id);
+    }
+
 }
