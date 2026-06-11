5e212952f3707f50f5d21440f80990bab588cce4
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-06-11 22:31:22 +0100

    go from jpa to spring data jpa

--- src/main/java/com/ismail/springbootDB/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/ismail/springbootDB/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
@@ -3,6 +3,7 @@ package com.ismail.springbootDB.learn_jpa_and_hibernate.course;
 import com.ismail.springbootDB.learn_jpa_and_hibernate.course.Course;
 import com.ismail.springbootDB.learn_jpa_and_hibernate.course.jdbc.CourseJdbcRepository;
 import com.ismail.springbootDB.learn_jpa_and_hibernate.course.jpa.CourseJpaRepository;
+import com.ismail.springbootDB.learn_jpa_and_hibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,15 +14,18 @@ public class CourseCommandLineRunner implements CommandLineRunner {
     //@Autowired
     //private CourseJdbcRepository courseJdbcRepository;
 
+    //@Autowired
+    //private CourseJpaRepository courseJpaRepository;
+
     @Autowired
-    private CourseJpaRepository courseJpaRepository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        courseJpaRepository.insert(new Course(1, "Learn AWS", "Ismail"));
-        courseJpaRepository.insert(new Course(2, "Learn AZURE", "Ismail"));
-        courseJpaRepository.insert(new Course(3, "Learn DevOps", "Ismail"));
-        courseJpaRepository.deleteById(2);
-        System.out.println(courseJpaRepository.findById(1));
+        repository.save(new Course(1, "Learn AWS", "Ismail"));
+        repository.save(new Course(2, "Learn AZURE", "Ismail"));
+        repository.save(new Course(3, "Learn DevOps", "Ismail"));
+        repository.deleteById(2L);
+        System.out.println(repository.findById(1L));
     }
 }
---
+++ src/main/java/com/ismail/springbootDB/learn_jpa_and_hibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -0,0 +1,7 @@
+package com.ismail.springbootDB.learn_jpa_and_hibernate.course.springdatajpa;
+
+import com.ismail.springbootDB.learn_jpa_and_hibernate.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+}
